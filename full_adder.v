731d4bc4b8f2867f73b152e576661ae189f74379
vkadu456 <vkadu456@gmail.com>
2025-07-27 17:23:38 +0530

    full_adder.v
    Full Adder using two half adder

--- full_adder.v
+++ full_adder.v
@@ -1,23 +1,7 @@
 /********************************************************************************************
-Copyright 2019 - Maven Silicon Softech Pvt Ltd. 
- 
-All Rights Reserved.
-
-This source code is an unpublished work belongs to Maven Silicon Softech Pvt Ltd.
-
-It is not to be shared with or used by any third parties who have not enrolled for our paid training 
-
-courses or received any written authorization from Maven Silicon.
-
-
-Webpage     :      www.maven-silicon.com
-
 Filename    :	   full_adder.v   
-
 Description :      One bit Full adder Design
-
-Author Name :      Susmita
-
+Author Name :      Vedant Kadu
 Version     : 	   1.0
 *********************************************************************************************/
 
