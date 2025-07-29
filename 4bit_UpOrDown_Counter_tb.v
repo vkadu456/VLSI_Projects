dd5e1f51673df785d19f5953430e319346856d65
Vedant Kadu <vkadu456@gmail.com>
2025-07-29 20:05:27 +0530

    4bit_UpOrDown_Counter_tb.v

---
+++ 4bit_UpOrDown_Counter_tb.v
@@ -0,0 +1,39 @@
+module counter_tb;
+
+    // Inputs
+    reg Clk;
+    reg reset;
+    reg UpOrDown;
+
+    // Outputs
+    wire [3:0] Count;
+
+    // Instantiate the Unit Under Test (UUT)
+    upordown_counter uut (
+        .Clk(Clk), 
+        .reset(reset), 
+        .UpOrDown(UpOrDown), 
+        .Count(Count)
+    );
+
+//Generate clock with 10 ns clk period.
+    initial Clk = 0;
+    always #5 Clk = ~Clk;
+    
+    initial begin
+        // Apply Inputs
+        reset = 0;
+        UpOrDown = 0;
+        #300;
+        UpOrDown = 1;
+      #300;
+        reset = 1;
+        UpOrDown = 0;
+        #100;
+        reset = 0;  
+    end
+	  initial 
+		begin
+      $monitor("Count= %b, reset= %b, UpOrDown= %b",Count, reset, UpOrDown);
+      end
+endmodule
