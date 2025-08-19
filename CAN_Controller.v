82f5a98e6442f01e34b6250ec3e950f02fc15378
Vedant Kadu <vkadu456@gmail.com>
2025-08-19 20:14:57 +0530

    CAN_Controller.v

--- CAN_Controller.v
+++ CAN_Controller.v
@@ -1,3 +1,9 @@
+/********************************************************************************************
+Filename    :	   can_controller.v   
+Description :      CAN Controller using verilog
+Author Name :      Vedant Kadu
+Version     : 	   1.0
+*********************************************************************************************/
 module can_controller (
     input clk,
     input reset,
